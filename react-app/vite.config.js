846d103f50900144fe613fb45fafd498a2233721
David Vera <dsvera.a@gmail.com>
2024-08-08 18:20:09 -0400

    vite config

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/iacc-01/react-app",
+  
+  base:"/react-app",
   plugins: [react()],
 })
